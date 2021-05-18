db740bd16ac50529fb81b7d030b7ffd66c15106e
Manu <manu@estadologico.com>
2021-05-18 22:02:25 +0200

    Movie scale when hover

--- src/components/Movie/index.js
+++ src/components/Movie/index.js
@@ -7,12 +7,10 @@ export function Movie({ movie }) {
   const { id, title, vote_average, poster_path } = movie
 
   return (
-    <div className="Movie">
-      <Link className="Movie__link" to={`/movie/${id}`}>
-        <div className="Movie__poster">
-          <MoviePoster posterPath={poster_path} />
-        </div>
-      </Link>
-    </div>
+    <Link className="Movie" to={`/movie/${id}`}>
+      <div className="Movie__poster">
+        <MoviePoster posterPath={poster_path} />
+      </div>
+    </Link>
   )
 }
