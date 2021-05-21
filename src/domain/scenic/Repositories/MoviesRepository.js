6b22b5435a0a072e5567faa71ab9c0c45b883484
Manu <manu@estadologico.com>
2021-05-21 22:44:14 +0200

    Remove unnecessary fromApiTo func

--- src/domain/scenic/Repositories/MoviesRepository.js
+++ src/domain/scenic/Repositories/MoviesRepository.js
@@ -2,40 +2,9 @@ import { Repository } from '../../domain.js'
 import { FromMovieToEntityMapper } from "../Mappers/FromMovieToEntityMapper"
 import { config } from '../../config.js'
 
-// Convertir en Mapper
-// Devolver "Entity"
-const fromApiTo = (apiResponse) => {
-  const { results = [] } = apiResponse
-  return results
-}
-
 export class MoviesRepository extends Repository {
   static create() {
     return new MoviesRepository()
-  }
-
-  async getTrendingMovies() {
-    const apiURL = `${config.apiURL}/trending/movie/week?api_key=${config.apiKey}`
-
-    return fetch(apiURL)
-      .then((res) => res.json())
-      .then(fromApiTo)
-  }
-
-  async getTopRatedMovies() {
-    const apiURL = `${config.apiURL}/movie/top_rated?api_key=${config.apiKey}`
-
-    return fetch(apiURL)
-      .then((res) => res.json())
-      .then(fromApiTo)
-  }
-
-  async getMoviesByKeywords({ keywords }) {
-    const apiURL = `${config.apiURL}/search/movie?language=en-US&query=${keywords}&api_key=${config.apiKey}`
-
-    return fetch(apiURL)
-      .then((res) => res.json())
-      .then(fromApiTo)
   }
 
   async getMovie({ movieID }) {
@@ -44,20 +13,41 @@ export class MoviesRepository extends Repository {
     const response = await fetch(apiURL)
       .then((response) => response.json())
 
-    // TODO GET DIRECTORS!
-    // const getDirectors = (response) => {
-    //   const directors = []
-    //   response.credits.crew.forEach(function (entry) {
-    //     if (entry.job === 'Director') {
-    //       directors.push(entry.name)
-    //     }
-    //   })
-    //   return directors.join(', ')
-    // }
-
     const movieEntityMapper = FromMovieToEntityMapper.create()
-    const responseEntity = movieEntityMapper.map(response)
 
-    return responseEntity
+    return movieEntityMapper.map(response)
   }
+
+  async getTrendingMovies() {
+    const apiURL = `${config.apiURL}/trending/movie/week?api_key=${config.apiKey}`
+
+    const response = await fetch(apiURL)
+      .then((response) => response.json())
+
+    const { results = [] } = response
+
+    return results
+  }
+
+  async getTopRatedMovies() {
+    const apiURL = `${config.apiURL}/movie/top_rated?api_key=${config.apiKey}`
+
+    const response = await fetch(apiURL)
+      .then((response) => response.json())
+
+    const { results = [] } = response
+
+    return results
+  }
+
+  async getMoviesByKeywords({ keywords }) {
+    const apiURL = `${config.apiURL}/search/movie?language=en-US&query=${keywords}&api_key=${config.apiKey}`
+
+    const response = await fetch(apiURL)
+      .then((response) => response.json())
+
+    const { results = [] } = response
+
+    return results
+  }  
 }
