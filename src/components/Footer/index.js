56df449d01422389345924f6ce84d205f97ca78b
Manu Morante <manu@estadologico.com>
2025-02-09 20:54:45 +0100

    Update Footer component content and remove unused styles

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,15 +1,10 @@
 import React from 'react'
-import './styles.scss'
 
 export const Footer = () => {
   return (
     <div className="Footer">
       <div className="container">
-        <p>
-          Copyright © 2021 Manu Morante.
-          <br /> Licencia Creative Commons Atribución-CompartirIgual 4.0
-          Internacional
-        </p>
+        <p>Made with love by Manu Morante in 2020</p>
       </div>
     </div>
   )
